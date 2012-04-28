8315a79978e890869c3963eda48904b0aedf561f
mallowlabs <mallowlabs@gmail.com>
2012-04-28 22:53:25 +0900

    Implemented: index.scala.html refs #1135

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -2,10 +2,66 @@ package controllers
 
 import play.api._
 import play.api.mvc._
+import models._
+import java.net.URI
 
 object Application extends Controller {
+
   def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+    val activities = List(
+      // TODO remove mock
+      Activity(
+        id = "https://codefirst.org/redmine/issues/935#change-2142",
+        title = "機能 #935 (担当): 検索機能の高速化",
+        body = "igo-ruby + ipa 辞書で実装中<br />実測でDBの使用容量が2倍、検索速度が1.5倍<br /><a class='https://codefirst.org/redmine/users/6'>下村/翔</a>",
+        createdAt = null,
+        source = "redmine",
+        project = "AsakusaSatellite",
+        url = Some(new URI("https://codefirst.org/redmine/issues/935#change-2142")),
+        icon_url = Some(new URI("http://dashbozu.herokuapp.com/assets/images/icons/redmine-shimo.png"))
+      ),
+      Activity(
+        id = "https://codefirst.org/redmine/issues/505#change-2141",
+        title = "機能 #505 (却下): Skypeブリッジ",
+        body = " Skype はオワコンなのでとりあえず却下します<br />",
+        createdAt = null,
+        source = "redmine",
+        project = "AsakusaSatellite",
+        url = Some(new URI("https://codefirst.org/redmine/issues/505#change-2141")),
+        icon_url = Some(new URI("http://dashbozu.herokuapp.com/assets/images/icons/redmine-shimo.png"))
+      ),
+      Activity(
+        id = "https://codefirst.org/redmine/issues/505#change-2141",
+        title = "ビルド #599 (2012/04/25 23:41:54)",
+        body = " Changes <ol>  <li>Removed: img border on IE refs #1125 (detail / redmineweb)</li> </ol> リモートホスト127.0.0.1が実行(git)",
+        createdAt = null,
+        source = "jenkins",
+        project = "AsakusaSatellite",
+        url = Some(new URI("https://codefirst.org/redmine/issues/505#change-2141")),
+        icon_url = Some(new URI("http://dashbozu.herokuapp.com/assets/images/icons/jenkins-success.png"))
+      ),
+      Activity(
+        id = "https://codefirst.org/redmine/issues/505#change-2141",
+        title = "ビルド #65 (2012/04/25 22:18:57)",
+        body = "Changes <ol> <li>テスト整備 refs #1116 (detail)</li> <li>push/pullに--forceオプションんを追加 refs #1116 (detail)</li> </ol> リモートホスト127.0.0.1が実行(git)",
+        createdAt = null,
+        source = "jenkins",
+        project = "AtsutaKatze",
+        url = Some(new URI("https://codefirst.org/redmine/issues/505#change-2141")),
+        icon_url = Some(new URI("http://dashbozu.herokuapp.com/assets/images/icons/jenkins-success.png"))
+      ),
+      Activity(
+        id = "76b91763",
+        title = "commit 76b91763",
+        body = "bugfix: disable redmine ticket link",
+        createdAt = null,
+        source = "git",
+        project = "AsakusaSatellite",
+        url = Some(new URI("https://codefirst.org/redmine/issues/505#change-2141")),
+        icon_url = Some(new URI("http://dashbozu.herokuapp.com/assets/images/icons/redmine-shimo.png"))
+      )
+    )
+    Ok(views.html.index(activities))
   }
 
   def hook = Action {
