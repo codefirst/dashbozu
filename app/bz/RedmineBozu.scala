7673a46ea84ec3989ca26066aad31e1c9a60dc92
SHIMOMURA Sho <graueule@gmail.com>
2012-04-29 00:07:34 +0900

    implement RedmineBozu refs #1138

---
+++ app/bz/RedmineBozu.scala
@@ -0,0 +1,27 @@
+package bz
+
+import scala.xml._
+import dispatch._
+import models._
+import java.text.SimpleDateFormat
+import java.net.URI
+
+class RedmineBozu extends Bozu {
+  def get(params : Map[String, String]) : List[Activity] = {
+    params.get("url").toList.flatMap(urlstring =>
+      Http( url(urlstring) <> {
+        elem => (elem \\ "entry").map(entry => {
+            val id        = (entry \ "id").text
+            val title     = (entry \ "title").text
+            val body      = (entry \ "body").text
+            val createdAt = new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss+09:00").
+                              parse((entry \ "updated").text)
+            val source    = "redmine"
+            val project   = (entry \ "id").text.split(" - ")(0)
+            val url       = Some(new URI((entry \ "link" \ "@href").text))
+            val iconUrl   = None
+            Activity(id, title, body, createdAt, source, project, url, iconUrl)
+        }).toList
+      }))
+  }
+}
