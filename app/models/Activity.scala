9f861c40fc9cacdd610d9259393235abf5493a48
mzp <mzpppp@gmail.com>
2012-04-28 18:11:39 +0900

    activityクラス追加 refs #1132

---
+++ app/models/Activity.scala
@@ -0,0 +1,14 @@
+package models
+import java.net.URI
+import java.util.Date
+
+sealed case class Activity(
+  id        : String,
+  title     : String,
+  body      : String,
+  createdAt : Date,
+  source    : String,
+  project   : String,
+  url       : Option[URI],
+  icon_url  : Option[URI]
+)
