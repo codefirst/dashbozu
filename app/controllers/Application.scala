d23a4539bf9e6c7938f2b9c16c265c398c9e42bd
banjun <banjun@gmail.com>
2012-04-29 18:16:13 +0900

    removed sortWith in Application refs #1132

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -10,7 +10,7 @@ object Application extends Controller {
 
   def index = Action {
     val activities =
-      ActivityDB.findAll.sortWith((x,y) => x.createdAt.getTime > y.createdAt.getTime)
+      ActivityDB.findAll
     Ok(views.html.index(activities.toList))
   }
 
