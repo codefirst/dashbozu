0c2068e4a2c765e6e7770e1ab66638be772764c8
mzp <mzpppp@gmail.com>
2012-04-30 07:19:58 +0900

    remove dead code refs #1192

--- app/controllers/ActivityPusher.scala
+++ app/controllers/ActivityPusher.scala
@@ -15,18 +15,6 @@ object ActivityPusher {
     secret <- Play.configuration.getString("pusher.secret")
   } Pusher.init(id, key, secret)
 
-  val act = Activity(
-    id = "https://codefirst.org/redmine/issues/935#change-2142",
-    title = "機能 #935 (担当): 検索機能の高速化",
-    body = "igo-ruby + ipa 辞書で実装中<br />実測でDBの使用容量が2倍、検索速度が1.5倍<br /><a class='https://codefirst.org/redmine/users/6'>下村/翔</a>",
-    createdAt = new java.util.Date(),
-    source = "jenkins",
-    project = "AsakusaSatellite",
-    url = Some(new URI("https://codefirst.org/redmine/issues/935#change-2142")),
-    iconUrl = Some(new URI("http://dashbozu.herokuapp.com/assets/images/icons/redmine.png"))
-  )
-
-
   def publish(activity : Activity) = {
     val html =
       views.html.activity(activity)
