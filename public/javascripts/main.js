617d71db703b58089089193d919190dc10b793d8
mallowlabs <mallowlabs@gmail.com>
2012-04-29 22:20:21 +0900

    Added: WebSocket status refs #1154

--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -8,10 +8,18 @@
     WEB_SOCKET_DEBUG = true;
 
     var pusher = new Pusher('a8a3a7b9b972e4107437');
+    pusher.connection.bind('initialized', function(data) {
+        $(".brand").css("color", "#3F9FD9");
+    });
+    pusher.connection.bind('connected', function(data) {
+        $(".brand").css("color", "#fff");
+    });
+    pusher.connection.bind('disconnected', function(data) {
+        $(".brand").css("color", "#3F9FD9");
+    });
+
     var channel = pusher.subscribe('activity');
-    console.log("hi");
     channel.bind('new', function(data) {
-        console.log(data);
         var elem = $(data.html);
         elem.hide();
         $(".activities").prepend(elem);
